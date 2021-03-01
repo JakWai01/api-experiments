589ca8f54aea83f02e54ac3a3ae366708ae3210a
JakWai01 <jakob.waibel@web.de>
2021-03-01 21:43:53 +0100

    feat: Handle path variables

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 )
 
 type Note struct {
+	Id string `json:"Id"`
 	Title string `json:"title"`
 	Content string `json:"content"`
 }
@@ -26,6 +27,7 @@ func handleRequests() {
 
 	r.HandleFunc("/", homepage)
 	r.HandleFunc("/notes", returnAllNotes)
+	r.HandleFunc("/notes/{id}", returnSingleNote)
 	log.Fatal(http.ListenAndServe(":10000", r))
 }
 
@@ -34,9 +36,22 @@ func returnAllNotes(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(Notes)
 }
 
+func returnSingleNote(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("returnSingleNote")
+	
+	vars := mux.Vars(r)
+	key := vars["id"]
+
+	for _, note := range Notes {
+		if note.Id == key {
+			json.NewEncoder(w).Encode(note)
+		}
+	}
+}
+
 func main() {
 	Notes = []Note{
-		Note{Title: "Hello World!", Content: "Just another dummy"},
+		Note{Id: "1", Title: "Hello World!", Content: "Just another dummy"},
 	}
 	handleRequests()
 }
